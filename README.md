0e7e0bda629bf7552c8a2a95748df9d906739282
Zoviet <alexandr@asustem.ru>
2025-07-07 14:35:17 +0400

    init

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # TLD
-List of top level domains (TLD)
+
+List of top level domains (TLD) in json format. 
+
+Source: https://data.iana.org/TLD/tlds-alpha-by-domain.txt
+
+Convert punycode names to UTF-8.
---
+++ parser.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#sudo apt install idn
+
+json="[]"
+
+curl -O https://data.iana.org/TLD/tlds-alpha-by-domain.txt
+
+sed -i '1d' tlds-alpha-by-domain.txt  
+
+while IFS= read -r line; do
+  line=$(idn --idna-to-unicode $line)
+  echo $line
+  json=$(echo "$json" | jq --arg str "${line,,}" '. + [$str]')
+done < tlds-alpha-by-domain.txt
+
+echo "$json" > tld.json
+
+sed -i ':a;N;$!ba;s/\n//g;s/ //g' tld.json
